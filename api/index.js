5acdf4df977301a911af4a9be1f766499143011e
raoufffffff <raouf1999hamoudi@gmail.com>
2024-07-31 07:15:48 +0200

    add auth folder order route

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
-ngodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const MongoDbKey = "mongodb+srv://raouf:rabah@cluster0.ayejlxj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 mongoose
     .connect(MongoDbKey)
