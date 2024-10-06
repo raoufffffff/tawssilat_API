c31ed78cc4d95308cc1e5063dca817fa2d85182f
raoufffffff <raouf1999hamoudi@gmail.com>
2024-10-06 03:41:37 +0200

    firt commit

--- midelWere/not/not.js
+++ midelWere/not/not.js
@@ -1,7 +1,71 @@
 const Restaurant = require('../../models/rest.Model');
+const { JWT } = require('google-auth-library');
+
+require('dotenv').config()
 
 
+function getAccessTokenAsync(key) {
+    return new Promise(function (resolve, reject) {
+        const jwtClient = new JWT(
+            key.client_email,
+            null,
+            key.private_key,
+            ['https://www.googleapis.com/auth/cloud-platform'],
+            null
+        );
+        jwtClient.authorize(function (err, tokens) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(tokens.access_token);
+        });
+    });
+}
 
+async function sendFCMv1Notification(a, b) {
+    const key = JSON.parse(process.env.FIREBASE_KEYS)
+    const firebaseAccessToken = await getAccessTokenAsync(key);
+    const deviceToken = a;
+
+    const messageBody = {
+        "message": {
+            "token": deviceToken,
+            "notification": {
+                "title": "you have a new order",
+                "body": `price: ${b} DA`,
+            },
+            "data": {
+                "channelId": "default",
+                "message": "Testing",
+                "title": "This is an FCM notification message",
+                "body": "{\"title\": \"new order\", \"body\": \"hello\"}",
+                "scopeKey": "@tawssilat/zbi",
+                "experienceId": "@tawssilat/zbi"
+            }
+        }
+
+    };
+
+    const response = await fetch(
+        `https://fcm.googleapis.com/v1/projects/zbii-407e9/messages:send`,
+        {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${firebaseAccessToken}`,
+                Accept: 'application/json',
+                'Accept-encoding': 'gzip, deflate',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(messageBody),
+        }
+    );
+
+    const readResponse = (response) => response.json();
+    const json = await readResponse(response);
+
+    console.log(`Response JSON: ${JSON.stringify(json, null, 2)}`);
+}
 
 
 const sendNot = async (restaurantId, orderDetails) => {
@@ -15,35 +79,8 @@ const sendNot = async (restaurantId, orderDetails) => {
         if (!restaurant.not) {
             throw new Error('Restaurant does not have a push notification token');
         }
+        sendFCMv1Notification(restaurant.not, orderDetails)
 
-        // Create the message payload for FCM
-        const message = {
-            token: restaurant.not, // Push token from the restaurant's database
-            notification: {
-                title: 'New Order',
-                body: `${orderDetails}`,  // Customize the body with order details
-            },
-            // APNs configuration for iOS
-            apns: {
-                payload: {
-                    aps: {
-                        sound: "a.wav", // Use the custom sound for iOS
-                    },
-                },
-            },
-            // Android configuration
-            android: {
-                priority: "high",
-                notification: {
-                    sound: "a.wav",  // Use the custom sound for Android
-                },
-            },
-        };
-
-        // Send the notification via Firebase Admin SDK
-        const response = await admin.messaging().send(message);
-        console.log("Successfully sent message:", response);
-        return { success: true, message: 'Notification sent successfully' };
 
     } catch (error) {
         console.error('Error sending notification:', error.message);
